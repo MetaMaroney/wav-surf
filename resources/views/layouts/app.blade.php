dc0e010756ac89b49a26b0bb55ed9653f4a8d3b1
MetaMaroney <ruben.chris.maroney@gmail.com>
2023-12-08 20:28:13 +0000

    Blade Layout and css style sheet

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Wav Surf</title>
+    <link rel="stylesheet" href="{{asset('main.css')}}">
+</head>
+
+<h1>App Layout Working</h1>
+
+<body>
+    <div id="div1"></div>
+    @yield('content')   
+</body>
+
+
+</html>
---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+   <h1>Profile.index</h1> 
+@endsection
+
