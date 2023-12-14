2723ca04b5bdf68731ebcecc9edb37fe491c2ee2
MetaMaroney <ruben.chris.maroney@gmail.com>
2023-12-14 18:24:27 +0000

    Added view for comments to profile

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,9 +9,11 @@
         <div class="profile-item" id="profile-item-1">
             <li>Username: {{$profile->username}}</li>
             <li>{{$profile->bio}}</li>
+            <input type="file">
         </div>
     </div>
     <div>
+        <h4>Posts</h4>
         @foreach ($profile->user->posts as $post)
             <div class="post">
                 <div class="postitem" id="posttop">                        
@@ -24,6 +26,17 @@
                 </div>
                 <div class="postitem" id="postbottom">
                     {{ $post->created_at }}
+                </div>
+            </div>
+        @endforeach
+        <h4>Comments</h4>
+        @foreach ($profile->user->comments as $comment)
+            <div class="post">
+                <div class="postitem" id="postmiddle">
+                    {{ $comment->content }}
+                </div>
+                <div class="postitem" id="postbottom">
+                    {{ $comment->created_at }}
                 </div>
             </div>
         @endforeach
